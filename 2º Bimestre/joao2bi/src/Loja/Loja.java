1516e33e480e786f7afe8e8cd9d5859e38a21314
João Pedro <126785897+Joao-Petter@users.noreply.github.com>
2023-05-11 11:29:31 -0300

    Update Loja.java

--- 2º Bimestre/joao2bi/src/Loja/Loja.java
+++ 2º Bimestre/joao2bi/src/Loja/Loja.java
@@ -1,43 +1,36 @@
-
 package Loja;
-import java.util.ArrayList;
 import java.util.List;
-public class Loja {
-    private String nomeloja;
-    private List <Clientes> cli = new ArrayList<>();
-    private List <Pedidos> ped = new ArrayList<>();
+
+public class Main {
 
 
-    public Loja(String nomel) {
-        this.nomeloja = nomel;
+    public static void main(String[] args)
+    {
+        //Clientes clic = new Clientes("lala",123,9981233);
+        Loja lol = new Loja("Marabraz");
+
+        Clientes cl1 = new Clientes("lala","126534","2837-20");
+        Clientes cl2 = new Clientes("ruanes","123564","92837-20");
+
+        lol.addped(new Pedidos(cl1,"maquina",122.2,232));
+        lol.addped(new Pedidos(cl1,"lixeira",99.4,20));
+
+        lol.addclie(cl1);
+        lol.addclie(new Clientes("Laura","608355636","119981233"));
+        lol.addclie(new Clientes("Marayo","133077595","119343933"));
+
+
+
+        cl1.showcliente();
+
+        for (Pedidos pd: lol.getPed()) {
+            if (pd.getClientes().getNome() == "lala"){
+                System.out.println(pd.getCodigo() +" "+ pd.getProduto() +" "+ pd.getValor());
+            }
+        }
+
+
 
     }
 
-    public String getNomel() {
-        return nomeloja;
-    }
-
-    public void setNomel(String nomel) {
-        this.nomeloja = nomel;
-    }
-
-    public List<Clientes> getCli() {
-
-        return cli;
-    }
-
-    public void addclie(Clientes cli) {
-
-        this.cli.add(cli);  //esse segundo cli é o parâmetro.
-    }
-
-    public List<Pedidos> getPed() {
-
-        return ped;
-    }
-
-    public void addped(Pedidos ped) {
-
-        this.ped.add(ped);
-    }
 }
